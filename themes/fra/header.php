3fda571bdb74cacad4170eeb58b5b727a161219b
franjavi01 <36012280+franjavi01@users.noreply.github.com>
2023-06-16 17:18:31 +0200

    linkear css

--- themes/fra/header.php
+++ themes/fra/header.php
@@ -1,10 +1,11 @@
 <!-- Fran -->
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <p>Esto es el header</p>

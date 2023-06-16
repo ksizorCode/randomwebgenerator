da0cecf32f5bedab6452ce1d4be422312407f6a6
Miguel <miguelestebn@gmail.com>
2023-06-16 15:31:46 +0200

    d

---
+++ themes/rau/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p>&copy; Copyright 2023 - Raul</p>
+</footer>
+</body>
+</html>
---
+++ themes/rau/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Titulo de apartado - Titulo de la web</title>
+
+</head>
+<body>
+
+<header>
+    <h1>Titulo de la web</h1>
+
+<nav>
+    <ul>
+        <li><a href="index.php">Inicio</a></li>
+    </ul>
+</nav>
+</header>

be498403a4eb13f9aa95999642ea6460def1457d
Ferastur <130502494+RichyGrand@users.noreply.github.com>
2023-06-16 17:14:46 +0200

    a

--- themes/ric/header.php
+++ themes/ric/header.php
@@ -12,7 +12,7 @@
     <header>
         <h1>"El impacto del deporte en la salud y el bienestar"</h1>
         <h2>El deporte desempeña un papel fundamental en la promoción de la salud </h2>
-        <img src="" alt="deporte">
+        <img src="https://images.unsplash.com/photo-1528543606781-2f6e6857f318?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1280&q=60" alt="deporte">
         <nav></nav>
         <ul></ul>
         <li><a href=""></a></li>

07bf3c67ace764f9d96bf6d8cd1b592843fbbab1
beatrabanco <127989170+beatrabanco@users.noreply.github.com>
2023-06-16 16:54:46 +0200

    main

--- themes/bea/main.php
+++ themes/bea/main.php
@@ -1,1 +1,17 @@
+<header>
 <h1>titulo</h1>
+<img src="img/" alt="">
+</header>
+<h2>subtitulo</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt unde soluta, porro sed dicta temporibus quis perferendis praesentium dolorum labore incidunt aperiam saepe culpa nobis modi facere dolorem atque quasi?</p>
+<aside>
+  <ul>
+    <li><a href="#">Opción 1</a></li>
+    <li><a href="#">Opción 2</a></li>
+    <li><a href="#">Opción 3</a></li>
+    <li><a href="#">Opción 4</a></li>
+  </ul>
+</aside>
+
+
+ 

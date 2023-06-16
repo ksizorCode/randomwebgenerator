e20445be618d3210d8124a38a165ecdd24c8c8a4
beatrabanco <127989170+beatrabanco@users.noreply.github.com>
2023-06-16 17:36:53 +0200

    css

--- themes/bea/main.php
+++ themes/bea/main.php
@@ -1,6 +1,10 @@
+<!-- Bea -->
+
 <header>
+    <div class="header">
 <h1>titulo</h1>
-<img src="img/logo.jpg" alt="">
+<img src="img/logo.jpg" alt="logo">
+</div>
 </header>
 <h2>subtitulo</h2>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt unde soluta, porro sed dicta temporibus quis perferendis praesentium dolorum labore incidunt aperiam saepe culpa nobis modi facere dolorem atque quasi?</p>

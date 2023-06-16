5a498a5bc5ab17848099ae8fdc6a8635cd64d063
Miguel <miguelestebn@gmail.com>
2023-06-16 18:07:41 +0200

    a

--- themes/mig/header.php
+++ themes/mig/header.php
@@ -1,6 +1,6 @@
 <!-- miguel -->
 <!DOCTYPE html>
-<html lang="es">
+<html lang="<?lang();?>">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">

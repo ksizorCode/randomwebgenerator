7eaa6843b271f1c4b3e8874233e88cae513040ed
SyderGZ <ericgalindo94@gmail.com>
2023-06-16 18:03:44 +0200

    g

---
+++ themes/xua/header.php
@@ -0,0 +1,27 @@
+<!-- xuan -->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Titulo de apartado - Titulo de la web</title>
+
+</head>
+<body>
+
+<header>
+    <h1>ROHAN</h1>
+<h2>Señor de los caballos</h2>
+
+
+<nav>
+    <ul>
+        <p>Sombragrís (Shadowfax en el original inglés) es un caballo ficticio del legendarium de J. R. R. Tolkien, que aparece en la novela El Señor de los Anillos. Sombragrís es uno de los mearas, los señores de los caballos, que eran domados y montados exclusivamente por los reyes de Rohan. Cabe destacar su enorme inteligencia, lealtad y, ante todo, velocidad y resistencia. Es de color gris plateado y puede entender la lengua de los hombres.
+
+Sombragrís fue prestado por el rey Théoden a Gandalf, aunque después de que el mago liberara al rey del embrujo de Saruman, este se lo ofreció como regalo en gesto de gratitud y ya no se separó del caballo. Sombragrís también dejó montar en algún momento a Pippin y a Gimli el Enano, aunque ambas veces Gandalf iba montado también. Tras la Guerra del Anillo, Sombragrís fue con Gandalf a Aman.</p>
+
+</ul>
+</nav>
+<img src="https://i.etsystatic.com/33105279/r/il/a76a10/4194011056/il_794xN.4194011056_mew2.jpg" alt="">
+</header>
+<main>

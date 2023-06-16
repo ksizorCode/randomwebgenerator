fe56a9fb97e278de50d16fcf264b15f481b6a507
123Laura-2022 <116003487+123Laura-2022@users.noreply.github.com>
2023-06-16 17:55:24 +0200

    dfs

--- themes/lau/header.php
+++ themes/lau/header.php
@@ -10,9 +10,7 @@
 <body>
   <header>
     <nav>
-   
-
-        <img src="" alt="">
+      <img id="saludar" src="https://media.istockphoto.com/id/469207006/es/foto/hola-verano.webp?b=1&s=170667a&w=0&k=20&c=8JVR4CY_iaBmGvc07Lhq76SVEKHpjQzo_t33GYEwe2M=" alt="Mano saludando">
         <ul class="menu">
             <li><a href="Inicio.php">Inicio</a></li>
             <li><a href="Ofertas.php">Ofertas</a></li>
@@ -20,12 +18,14 @@
             <li><a href="Nosotros.php">Nosotros</a></li>
             <li><a href="Contacto.php">Contacto</a></li>
         </ul>
+
     </nav>
 <style>
     ul.menu{
       display:flex;
       justify-content:space-around;
-      background-image: linear-gradient(var(--l2));
+      background:var(--l2);
+      padding:10px;
   }
   ul.menu li{
       list-style: none;

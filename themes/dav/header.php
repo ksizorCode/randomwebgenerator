3de26c5f0ad77baa5dbe2ad61c188bb9e1323cb0
David4888 <74598998+David4888@users.noreply.github.com>
2023-06-16 20:35:48 +0200

    Dav

---
+++ themes/dav/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi paginuca</title>
+</head>
+<body>
+<header>
+    <h1>Que se yo que poner</h1>
+
+<nav>
+    <ul>
+        <li><a href="index.php">Inicio</a></li>
+        <li><a href="index.php">Nosotros</a></li>
+        <li><a href="index.php">Contacto</a></li>
+    </ul>
+</nav>
+</header>
+</body>
+</html>

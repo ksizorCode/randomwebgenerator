d2afdef9c76895bce963d0c70c15b87d8505148e
KateFlecher <127534593+KateFlecher@users.noreply.github.com>
2023-06-16 16:43:47 +0200

    jss

--- themes/jes/header.php
+++ themes/jes/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Titulo de apartado - Titulo de la web</title>
+    <title>Sólo queda una semana</title>
 
 </head>
 <body>

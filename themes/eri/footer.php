ce4ea8651ff2fecf103230c2b1f3e0e10e4341b2
David4888 <74598998+David4888@users.noreply.github.com>
2023-06-16 17:30:47 +0200

    das

---
+++ themes/eri/footer.php
@@ -0,0 +1,22 @@
+<p id="countDown"></p>
+
+</main>
+<footer>
+<div class="pie">
+    <div>
+      <p>&copy; Copyright 2023</p>
+    </div>
+    
+    <div>
+      <img src="https://sticker.nyc3.cdn.digitaloceanspaces.com/20210830/file_3245736_512x512.webp" alt="">
+    </div>
+
+    <div>
+    <p>developed by Eric Galindo</p>
+    </div>
+  </div>
+  <a href="index.php"><? el_titulo(); ?></a>
+</footer>
+
+</body>
+</html>

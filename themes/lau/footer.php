44467c24dba878444aad609c867467e97ec8d68d
123Laura-2022 <116003487+123Laura-2022@users.noreply.github.com>
2023-06-16 19:23:24 +0200

    Update footer.php

--- themes/lau/footer.php
+++ themes/lau/footer.php
@@ -2,7 +2,7 @@
   <footer>
     <img src="" alt="">
     <div id="pie">
-        <p>Este es un footer normal y corriete</p>
+        <p>Este es un footer normal y corriente</p>
     </div>
   <?php echo date('Y')?>
   </footer>  

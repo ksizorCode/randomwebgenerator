92de5dd24babe0ed0ab6ae6719cd9fcefc8d2dd4
Miguel <miguelestebn@gmail.com>
2023-06-16 16:55:08 +0200

    aa

--- ver.php
+++ ver.php
@@ -6,26 +6,26 @@ if(isset($_GET['nombre'])){
 
 ?>
 
-<form action="">
+<form action="ver.php" method="get">
     <label for="nombre">¿Quien eres?</label>
-    <input type="text" id="nombre" value="nombre">
+    <input type="text" id="nombre" name="nombre">
     <p>Recuerda que estamos trabajando sólo con 3 letras</p>
 </form>
 
 
 <style>
-<? include 'themes/'.$nombre.'style.css';?>
+<? include 'themes/'.$nombre.'/style.css';?>
 </style>
 
 
 
 
 <p>Esto es tu header:</p>
-<? include 'themes/'.$nombre.'header.php';?>
+<? include 'themes/'.$nombre.'/header.php';?>
 
 <p>Esto es tu main:</p>
-<? include 'themes/'.$nombre.'main.php';?>
+<? include 'themes/'.$nombre.'/main.php';?>
 
 <p>Esto es tu footer:</p>
-<? include 'themes/'.$nombre.'footer.php';?>
+<? include 'themes/'.$nombre.'/footer.php';?>
 

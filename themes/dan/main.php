8a95786b43be06fb63565114c521db88f6d638b1
Miguel <miguelestebn@gmail.com>
2023-06-19 19:46:30 +0200

    a

--- themes/dan/main.php
+++ themes/dan/main.php
@@ -1,5 +1,11 @@
 <section>
+  <h1><? e('title')?></h1>
+
 <img id="foto" src="<?e('img1')?>" alt="">
 <img id="foto" src="<?e('img2')?>" alt="">
 <img id="foto" src="<?e('img3')?>" alt="">
-  </section>
+
+<p><? e('txt1')?></p>
+<p><? e('txt2')?></p>
+
+</section>

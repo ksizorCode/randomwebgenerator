c0f88cf645028e45c59f0eb97ea764aa6e3eab87
123Laura-2022 <116003487+123Laura-2022@users.noreply.github.com>
2023-06-19 17:10:42 +0200

    sdd

--- themes/lau/header.php
+++ themes/lau/header.php
@@ -16,9 +16,9 @@
       
       <ul class="menu">
         
-      <li><div id="mascara"> 
-        <img id="fotoale" src="https://media.istockphoto.com/id/469207006/es/foto/hola-verano.webp?b=1&s=170667a&w=0&k=20&c=8JVR4CY_iaBmGvc07Lhq76SVEKHpjQzo_t33GYEwe2M=" alt="Mano saludando">
-        </div></li>
+      <li> 
+        <img src="https://media.istockphoto.com/id/469207006/es/foto/hola-verano.webp?b=1&s=170667a&w=0&k=20&c=8JVR4CY_iaBmGvc07Lhq76SVEKHpjQzo_t33GYEwe2M=" alt="Mano saludando">
+      </li>
             <li><a href="Inicio.php">Inicio</a></li>
             <li><a href="Ofertas.php">Ofertas</a></li>
             <li><a href="Productos.php">Productos</a></li>
--- themes/lau/main.php
+++ themes/lau/main.php
@@ -3,6 +3,11 @@
     <h1>Esta es una aplicación hecha entre todos</h1>
     <img src="https://media.istockphoto.com/id/1392016982/es/foto/grupo-mixto-de-empresarios-sentados-alrededor-de-una-mesa-y-hablando.jpg?s=612x612&w=0&k=20&c=K5htjbC66KE4KL7R4Ca6rPxU1I7PkwffSq27gbOIGzo=" alt="Foto grupal">
     <h2>A ver si sale bien todo</h2>
+    <ul>
+        <li><img src="img/001.jpg" alt=""></li>
+        <li><img src="img/002.jpg" alt=""></li>
+        <li><img src="img/003.jpg" alt=""></li>
+    </ul>
     <section>
     <article>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, rem.</p>

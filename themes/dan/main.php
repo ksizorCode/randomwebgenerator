01985cf76bd16ef6b71ac4dff313c8e0d6336c88
Miguel <miguelestebn@gmail.com>
2023-06-19 19:44:44 +0200

    a

--- themes/dan/main.php
+++ themes/dan/main.php
@@ -2,5 +2,4 @@
 <img id="foto" src="https://images.unsplash.com/photo-1678297405489-d25d9f5bdff0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="">
 <img id="foto" src='https://images.unsplash.com/photo-1678308860535-210ff0f57af6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80' >
 <img id="foto" src='https://images.unsplash.com/photo-1678329887232-a48991da8286?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80' >
-<img id="foto" src='https://images.unsplash.com/photo-1677741448429-aea31dd26a59?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80' >
   </section>

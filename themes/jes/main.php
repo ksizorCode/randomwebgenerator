ed7cec6b48b66977bfb9cd8a9882afa0f3857868
KateFlecher <127534593+KateFlecher@users.noreply.github.com>
2023-06-19 17:09:39 +0200

    chi

--- themes/jes/main.php
+++ themes/jes/main.php
@@ -1,5 +1,5 @@
 <!-- Jess -->
 
-<img src="img/001.jpg" alt="WonderWoman">
-<img src="img/002.jpg" alt="SuperGirl">
-<img src="img/003.jpg" alt="ViudaNegra">
+<img src="/themes/jes/img/001.jpg"alt="WonderWoman">
+<img src="/themes/jes/img/002.jpg" alt="SuperGirl">
+<img src="/themes/jes/img/003.jpg" alt="ViudaNegra">

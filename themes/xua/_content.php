e30c2d3f51a1db25f0e78e708adfe50c8f79b4f2
beatrabanco <127989170+beatrabanco@users.noreply.github.com>
2023-06-19 18:42:34 +0200

    4

--- themes/xua/_content.php
+++ themes/xua/_content.php
@@ -4,7 +4,7 @@
 //Array que  que almacena todos los datos
 
 $dato =[
-    'titulo'=>"Batman regresa en la última peli de Flash",
+    'titulo'=>"Rhojan",
     'subtitulo'=>'La última película del estudio Warner Broders promete una buena dosis de nostalgia a los fans del comic de DC.',
     'txt1'=>'Han pasado más de 34 años desde que Tim Burton estrenó la primera adaptación cinematográfica del super heroe de la negra capa. ',
     'txt2'=>'Lorem ipsum dolor 2',

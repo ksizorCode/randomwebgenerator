d0d0def169586a4d130cb39599c2bb697d254b96
123Laura-2022 <116003487+123Laura-2022@users.noreply.github.com>
2023-06-19 20:46:02 +0200

    21

--- themes/lau/main.php
+++ themes/lau/main.php
@@ -4,9 +4,9 @@
     <img src="https://media.istockphoto.com/id/1392016982/es/foto/grupo-mixto-de-empresarios-sentados-alrededor-de-una-mesa-y-hablando.jpg?s=612x612&w=0&k=20&c=K5htjbC66KE4KL7R4Ca6rPxU1I7PkwffSq27gbOIGzo=" alt="Foto grupal">
     <h2><? e('subtitulo')?></h2>
     <ul>
-        <li><?php e($dato['img1'])?></li>
-        <li><?php e($dato['img2'])?></li>
-        <li><?php e($dato['img3'])?></li>
+        <li><?php e('img1')?></li>
+        <li><?php e('img2')?></li>
+        <li><?php e('img3')?></li>
     </ul>
     <section>
     <article>
